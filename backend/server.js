9d64a0be326270fa4e74044451f37d1db5392a7b
e1901012 <illya.yushchenko@gmail.com>
2020-08-19 12:44:27 +0300

    first commit

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-//'mongodb://localhost/todo
 
-    mongoose.connect('mongodb+srv://testuser:6543210@rest-10ids.mongodb.net/rest?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
+    mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true, useUnifiedTopology: true });
     const db = mongoose.connection;
     db.on('error', (error) => console.error(error));
     db.once('open', () => console.log('Connected to Database'));
